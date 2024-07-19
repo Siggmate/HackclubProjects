5349bcc97960630ba13c609964e4fa4dd794c5ec
Siggmate <elvinthebestgamer@gmail.com>
2024-07-19 18:43:15 +0200

    Update ship.gd

--- SpaceshipProject/Scripts/ship.gd
+++ SpaceshipProject/Scripts/ship.gd
@@ -3,14 +3,22 @@ extends CharacterBody2D
 var speed := 150
 var acc := 2
 var friction := 5
+var hp := 100
+var invincible := false
 @onready var hurtbox: Area2D = $Hurtbox
+@onready var animations = $Animations
+@onready var healthbar = $Node2D/Healthbar
+@onready var inv_timer = $InvTimer
+@onready var death_particle = $DeathParticle
 
 func _ready():
+	healthbar.set_value(hp)
 	hurtbox.body_entered.connect(_on_hurtbox_body_entered)
 
 func _process(delta):
 	move()
-
+	if hp <= 0:
+		death()
 
 func move():
 	var direction := Input.get_vector("left", "right", "up", "down")
@@ -27,4 +35,15 @@ func apply_friction():
 	velocity = velocity.move_toward(Vector2.ZERO, friction)
 	
 func _on_hurtbox_body_entered(body):
-	queue_free()
+	if invincible == false:
+		invincible = true
+		inv_timer.start()
+		animations.play("flash")
+		hp -= body.dmg
+		healthbar.set_value(hp)
+
+func _on_inv_timer_timeout():
+	invincible = false
+
+func death():
+	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
