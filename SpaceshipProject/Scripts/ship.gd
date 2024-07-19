3671c2c9cd09a95b3a5b7a1181a08e9e53cfe257
Siggmate <elvinthebestgamer@gmail.com>
2024-07-19 13:12:46 +0200

    Update ship.gd

--- SpaceshipProject/Scripts/ship.gd
+++ SpaceshipProject/Scripts/ship.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 var speed := 150
-var directions := ["left", "right", "up", "down"]
 var acc := 2
 var friction := 5
+@onready var hurtbox: Area2D = $Hurtbox
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	hurtbox.body_entered.connect(_on_hurtbox_body_entered)
+
 func _process(delta):
 	move()
+
 
 func move():
 	var direction := Input.get_vector("left", "right", "up", "down")
@@ -22,3 +25,6 @@ func accelerate(direct: Vector2):
 	
 func apply_friction():
 	velocity = velocity.move_toward(Vector2.ZERO, friction)
+	
+func _on_hurtbox_body_entered(body):
+	queue_free()
